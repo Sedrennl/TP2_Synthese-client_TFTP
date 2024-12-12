654874b90f11977f5e97124c4ec4f43896319fbd
Sédrenn Labrousse <161329715+Sedrennl@users.noreply.github.com>
2024-12-12 11:08:06 +0100

    Update q1.c

--- gettftp/q1.c
+++ gettftp/q1.c
@@ -9,11 +9,13 @@
 
 int main(int argc, char *argv[])
 {
+    //Error test
     if (argc != 4)
     {
         fprintf(stderr, "Usage: %s  <Server ip address> <Port> <File_name> \n", argv[0]);
         exit(EXIT_FAILURE);
     }
+    
     char *server_ip = argv[1];
     char *server_port = argv[2];
     char *file_name = argv[3];
