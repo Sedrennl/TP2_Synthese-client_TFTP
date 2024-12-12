60b73c7c4a89c768e9141bef070188405171fe9d
jlblt <julien.blaret@ensea.fr>
2024-12-12 11:03:55 +0100

    Q2 faite, à vérifier lors de la Q3

--- gettftp/main.c
+++ gettftp/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<netdb.h>
+#include<sys/socket.h>
+
 int main(int argc, char *argv[])
 {
     if (argc != 4)
@@ -11,9 +17,19 @@ int main(int argc, char *argv[])
     char *server_ip = argv[1];
     char *server_port = argv[2];
     char *file_name = argv[3];
-    struct addrinfo hints; //sert à réstreindre la recherche
+
+
+
+    struct addrinfo hints; //sert à restreindre la recherche
     memset(&hints, 0, sizeof(hints)); // On met nul : pas de restriction;
-    struct addrinfo *res; // Pour pas que ça prenne trop de place, et parce que on ne peut pas faire &&res, alors que la fonction veut un **res
-    hints.ai_protocol = IPPROTO_TCP;
-    getaddrinfo(argv[1], NULL, &hints, &res);
+    struct addrinfo *res; // Résultats de la recherche,  permet les listes chaînées
+
+    //paramètrage du hints
+    hints.ai_protocol = IPPROTO_UDP;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int addrinfo = getaddrinfo(argv[1], NULL, &hints, &res);
+
+    int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 }
